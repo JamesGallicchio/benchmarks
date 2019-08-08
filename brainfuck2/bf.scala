bbfd13725198ae777d45fe510ba8788f63756bda
James Gallicchio <JamesGallicchio@users.noreply.github.com>
2019-08-07 21:24:47 -0400

    Improve bf.scala impl

--- brainfuck2/bf.scala
+++ brainfuck2/bf.scala
@@ -1,66 +1,30 @@
-abstract class Op
-case class Inc(v: Int) extends Op
-case class Move(v: Int) extends Op
-case class Print() extends Op
-case class Loop(loop: Array[Op]) extends Op
-case class Nop() extends Op
-
-class Tape() {
-  private var tape = Array(0)
-  private var pos  = 0
-
-  def get = tape(pos)
-  def inc(x: Int) = tape(pos) += x
-  def move(x: Int) = { 
-    pos += x
-    while (pos >= tape.length) { tape :+= 0 }
-  }
-}
-
-class Program(text: String) {
-  var ops = parse(text.iterator)
-
-  def parse(iterator: Iterator[Char]) : Array[Op] = {
-    var res = Array[Op]()
-    while (iterator.hasNext) {
-      val op = iterator.next() match {
-        case '+' => new Inc(1)
-        case '-' => new Inc(-1)
-        case '>' => new Move(1)
-        case '<' => new Move(-1)
-        case '.' => new Print()
-        case '[' => new Loop(parse(iterator))
-        case ']' => return res
-        case _ => new Nop()
-      }
-
-      op match {
-        case Nop() => ()
-        case _ => res :+= op
-      }
-    }
-
-    res
-  }
-
-  def run = _run(ops, new Tape())
-
-  def _run(program: Array[Op], tape: Tape) {
-    for (op <- program) op match {
-      case Inc(x) => tape.inc(x)
-      case Move(x) => tape.move(x)
-      case Loop(loop) => while (tape.get > 0) _run(loop, tape)
-      case Print() => print(tape.get.toChar)
-    }
-  }
-
-}
-
 object BrainFuck {
-  def time(f: => Any) = {
+  def run(prog: String): Unit = {
+    var idx = 0
+    var tape = new ArrayBuffer[Byte]()
+    var ptr = 0
+    var brackets = List[Int]()
+    
+    while (idx < prog.length) {
+      prog(idx) match {
+        case '+' => tape(ptr) += 1
+        case '-' => tape(ptr) += -1
+        case '>' => ptr += 1
+        case '<' => ptr -= 1
+        case '[' => brackets = idx :: brackets
+        case ']' => idx = brackets.head; brackets = brackets.tail
+        case '.' => print(tape(ptr).toChar)
+        case _   =>
+      }
+      idx += 1
+    }
+  }
+  
+  def time(f: => Any):  = {
     val s = System.nanoTime
     val ret = f
-    System.err.println("time: "+(System.nanoTime-s)/1e9+"s")
+    val e = System.nanoTime
+    System.err.println("time: "+(e-s)/1e9+"s")
     ret
   }
 
@@ -68,16 +32,15 @@ object BrainFuck {
     val text = scala.io.Source.fromFile(args(0)).mkString
 
     //warmup
-    System.err.print("warmup\n")
+    System.err.println("warmup")
     time {
       new Program(">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++").run
     }
     //
 
-    System.err.print("run\n")
+    System.err.println("run")
     time {
-      new Program(text).run
+      run(text)
     }
-
   }
 }
